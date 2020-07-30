4eb34ffd7e35b1f07da2a94ad111310cb8be900b
Андрей Гриценко <a.gritsenko87@yandex.ru>
2020-07-30 22:22:18 +0300

    Практическое задание 2

---
+++ Lesson_1_HomeWork_2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lesson_1_HomeWork_2
+{
+    class Program
+    {
+        delegate void Func();
+
+        private static readonly string address = Constants.Adress;
+        private static readonly string format = Constants.Format;
+
+        private static void WriteToConsole(string name, string description, int age)
+        {
+            Console.WriteLine(format, name, description, address, age);
+        }
+
+        private static void DummyFunc()
+        {
+            WriteToConsole("Андрей", "школьный друг", 30);
+        }
+
+        private static void DummyFuncAgain()
+        {
+            WriteToConsole("Александр", "брат", 32);
+        }
+
+        private static void DummyFuncMore()
+        {
+            WriteToConsole("Арина", "дочь", 2);
+        }
+
+        private static List<Func> GetFuncs()
+        {
+            return new List<Func>()
+            {
+                DummyFunc,
+                DummyFuncAgain,
+                DummyFuncMore
+            };
+        }
+
+        private static void MakeFunc(Func func)
+        {
+            string methodName = func.Method.Name;
+            Console.WriteLine($"Начало работы метода {methodName}");
+            func();
+            Console.WriteLine($"Окончание работы метода {methodName}");
+        }
+
+        private static void Main(string[] args)
+        {
+            List<Func> funcs = GetFuncs();
+            foreach (var func in funcs)
+            {
+                MakeFunc(func);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
