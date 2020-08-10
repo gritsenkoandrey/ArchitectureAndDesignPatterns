df470e73f8dd0dcc3cd2274a642f9ad53e921d46
Андрей Гриценко <a.gritsenko87@yandex.ru>
2020-08-10 17:34:03 +0300

    Lazy Initialization

---
+++ Lazy_Initialization_1/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Lazy_Initialization_1
+{
+    public class Client
+    {
+        //StringCollection stringCollection = new StringCollection();
+        Lazy<StringCollection> stringCollection = new Lazy<StringCollection>();
+
+        public void UseItem()
+        {
+            //stringCollection.GetItem();
+            stringCollection.Value.GetItem();
+            Console.WriteLine("UseItem");
+        }
+
+        public void PrintItem()
+        {
+            Console.WriteLine("PrintItem");
+        }
+    }
+}
