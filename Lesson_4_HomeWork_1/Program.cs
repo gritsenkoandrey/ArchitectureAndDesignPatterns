e362f4e0b54979b7b601e34d1c4513a53146478a
Андрей Гриценко <a.gritsenko87@yandex.ru>
2020-08-11 17:38:35 +0300

    Практическое задание к четвертому уроку

---
+++ Lesson_4_HomeWork_1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Lesson_4_HomeWork_1
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private static object _lock = new object();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock(_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
