67b3dba7d3efeecbf2503b61fa74a4cdbd02d2b1
Андрей Гриценко <a.gritsenko87@yandex.ru>
2020-08-15 15:25:59 +0300

    Lesson 5 HomeWork 2

---
+++ Lesson_5_HomeWork_2/ICalculate.cs
@@ -0,0 +1,7 @@
+﻿namespace Lesson_5_HomeWork_2
+{
+    public interface ICalculate
+    {
+        int Calculate(int num1, int num2);
+    }
+}
---
+++ Lesson_5_HomeWork_2/Math.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Lesson_5_HomeWork_2
+{
+    public class Math : ICalculate
+    {
+        private int _sum = 0;
+        private string _nameOperation;
+
+        public Math(string name)
+        {
+            _nameOperation = name;
+            Console.WriteLine($"Operation is {name}");
+        }
+
+        public int Calculate(int num1, int num2)
+        {
+            switch (_nameOperation)
+            {
+                case "+":
+                    return _sum = num1 + num2;
+
+                case "-":
+                    return _sum = num1 - num2;
+
+                case "*":
+                    return _sum = num1 * num2;
+
+                case "/":
+                    return _sum = num1 / num2;
+
+                default:
+                    Console.WriteLine($"Операция {_nameOperation} не опознана");
+                    break;
+            }
+            return _sum;
+        }
+    }
+}
