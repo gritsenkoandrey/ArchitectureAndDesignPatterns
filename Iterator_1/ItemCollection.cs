b7a07e89cf3f72949fa032192f97bd9ad046adec
Андрей Гриценко <a.gritsenko87@yandex.ru>
2020-08-17 14:38:32 +0300

    Iterator Pattern

---
+++ Iterator_1/ItemCollection.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Iterator_1
+{
+    public class ItemCollection<T> : IEnumerable<T>
+    {
+        private List<T> _items = new List<T>();
+        public int Count => _items.Count;
+
+        public IEnumerable<T> FrontToBack => this;
+
+        public IEnumerable<T> BackToFront
+        {
+            get
+            {
+                for (int i = Count - 1; i >= 0; i--)
+                {
+                    yield return _items[i];
+                }
+            }
+        }
+
+        public void Add(T t)
+        {
+            _items.Add(t);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return _items[i];
+            }
+        }
+
+        public IEnumerable<T> FromToStep(int from, int to, int step)
+        {
+            for (int i = from; i <= to; i = i + step)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Iterator_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Iterator_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new ItemCollection<Item>
+            {
+                new Item {Name = "Элемент 0"},
+                new Item {Name = "Элемент 1"},
+                new Item {Name = "Элемент 2"},
+                new Item {Name = "Элемент 3"},
+                new Item {Name = "Элемент 4"},
+                new Item {Name = "Элемент 5"}
+            };
+
+            Console.WriteLine("От начала к концу");
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("\nОт конца к началу");
+            foreach (var item in collection.BackToFront)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("\nОт 1 до 5 с шагом 2");
+            foreach (var item in collection.FromToStep(1, 5, 2))
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
