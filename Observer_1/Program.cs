80cb00edd171ff0b54e9109e24c0638c5d9a45b9
Андрей Гриценко <a.gritsenko87@yandex.ru>
2020-08-21 16:00:25 +0300

    Observer Pattern

---
+++ Observer_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var om = new ObservableMinerals();
+            var goldObserver = new GoldObserver();
+            var silverObserver = new SilverObserver();
+            var platinaObserver = new PlatinaObserver();
+            IEnumerable<Mineral> minerals = new MineralRepository().GetMinerals();
+
+            using (om.Subscribe(goldObserver))
+            {
+                using (om.Subscribe(silverObserver))
+                {
+                    using (om.Subscribe(platinaObserver))
+                    {
+                        foreach (var mineral in minerals)
+                        {
+                            om.Mineral = mineral;
+                        }
+                        om.Stop();
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Observer_1/SilverObserver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Observer_1
+{
+    /// <summary>
+    /// Наблюдатель за ценой серебра
+    /// </summary>
+    public class SilverObserver : IObserver<Mineral>
+    {
+        public void OnCompleted()
+        {
+            Console.WriteLine("Биржа закрыта.");
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine($"Ошибка: {error.Message}");
+        }
+
+        public void OnNext(Mineral mineral)
+        {
+            if (mineral.Name == "Серебро" && mineral.Price > 20)
+            {
+                Console.WriteLine($"Цена на серебро {mineral.Price} превысила 20$ за унцию");
+            }
+        }
+    }
+}
